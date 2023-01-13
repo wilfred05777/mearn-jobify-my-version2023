cebb55b68421210447fc29778a4963f7d5f0a05b
Wilfred <wilfred05777@gmail.com>
2023-01-13 12:59:33 +0800

    add normalize and  github repo

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import 'normalize.css' 
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -10,3 +11,5 @@ root.render(
   </React.StrictMode>
 );
 
+
+// https://github.com/wilfred05777/mearn-jobify-my-version2023
